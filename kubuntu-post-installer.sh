e086831c6bec651adf0b0f0b88b9c4008ca1b794
Braden Goodwin <6876787+bradentg@users.noreply.github.com>
2024-06-03 13:53:36 -0400

    Add files via upload

---
+++ kubuntu-post-installer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Brave Browser
+sudo apt install curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
+
+# Signal
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+  sudo tee /etc/apt/sources.list.d/signal-xenial.list
+
+# Timeshift
+sudo apt install timeshift
+
+# Snaps
+sudo snap install spotify discord
+sudo snap install code --classic
+sudo snap install intellij-idea-community --classic
+sudo snap install sublime-text --classic
+
+# Flatpak
+sudo apt install flatpak
+sudo apt install plasma-discover-backend-flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub md.obsidian.Obsidian
+flatpak install https://flathub.org/repo/appstream/org.gimp.GIMP.flatpakref
